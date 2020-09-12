de35afe38c1ee3755e66be500f6962d8d4ccf979
Danny-7 <danielaguiar@outlook.fr>
2020-09-12 20:48:28 +0200

    navbar almost ended, first section done

---
+++ js/header.js
@@ -0,0 +1,39 @@
+const header = document.querySelector(".main__header");
+const navigationBar = document.querySelector('.navigation');
+const navItems = document.querySelectorAll('.navigation>.navbar__item');
+const subMenus = document.querySelectorAll('.navbar__subMenu');
+const hamburgerMenu = document.querySelector('.mobile__menu__bar');
+const searchBtn = document.querySelector('.nav__search');
+const searchBar = document.querySelector('.searchBar');
+
+const positionTop = header.offsetHeight;
+
+if (positionTop !== 0) {
+    navigationBar.setAttribute('style', `top: ${positionTop}px`)
+}
+
+hamburgerMenu.addEventListener('click', () => {
+    navigationBar.classList.toggle('active');
+});
+
+searchBtn.addEventListener('click', () => {
+    searchBar.classList.toggle('show');
+    navigationBar.setAttribute('style', 'display: none');
+});
+
+const scrollFunction = () => {
+    if (window.innerWidth > 1300) {
+        if (window.pageYOffset > 0) {
+            subMenus.forEach(menu => menu.setAttribute('style', 'margin-top: 1em'));
+            navigationBar.classList.add('pt-1');
+            navItems.forEach(item => item.classList.add('pb-1'));
+        } else {
+            subMenus.forEach(menu => menu.setAttribute('style', 'margin-top: 3em'));
+            navigationBar.classList.remove('pt-1');
+            navItems.forEach(item => item.classList.remove('pb-1'));
+        }
+    }
+
+};
+
+window.addEventListener('scroll', scrollFunction);
